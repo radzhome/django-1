5674101f1c40660e8d450d3e1f8dc6ef038ed029
Rad W <radzhome@gmail.com>
2021-01-20 17:26:59 -0500

    change custom version

--- django/__init__.py
+++ django/__init__.py
@@ -1,8 +1,8 @@
 from django.utils.version import get_version
 
-VERSION = (2, 2, 18, 'alpha', 0)
+VERSION = (2, 2, 18, 'a', 0)
 
-__version__ = get_version(VERSION)
+__version__ = '2.2.18a'
 
 
 def setup(set_prefix=True):
