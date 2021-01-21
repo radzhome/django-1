1d759c6af7b3ae9244ba3a7c07e6525470075c4c
Rad W <radzhome@gmail.com>
2021-01-21 00:03:58 -0500

    Update __init__.py

--- django/__init__.py
+++ django/__init__.py
@@ -1,8 +1,8 @@
 from django.utils.version import get_version
 
-VERSION = (2, 2, 999, 'final', 0)
+VERSION = (2, 2, 9999, 'final', 0)
 
-__version__ = '2.2.999'
+__version__ = '2.2.9999'
 
 
 def setup(set_prefix=True):
